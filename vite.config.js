bd764c429cfad33cda85f1b641a97e1710e8fd24
Lorenzo Dick Schadeck <lorenzoschadeck@icloud.com>
2025-08-04 18:55:55 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( ),tailwindcss()],
-  base: "/portfolio-site/", 
+  base: "/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
